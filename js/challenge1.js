6a0d6b0af4c4a8ed96f56b2aff050ab1c971146a
Siddarth Jalagam <128750737+siddrick38@users.noreply.github.com>
2024-04-09 13:49:26 -0400

    Add files via upload

---
+++ js/challenge1.js
@@ -0,0 +1,9 @@
+// challenge 1
+
+let heading = document.querySelector("h1");
+heading.style.color = "purple";
+heading.style.marginLeft = "45%";
+
+document.body.addEventListener("dblclick", function () {
+  alert(new Date());
+});
